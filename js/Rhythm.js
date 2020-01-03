b3fd412e1c24cbe775020bc8214fde7bfb07bede
christypy <108753114@g.nccu.edu.tw>
2020-01-03 15:01:44 +0800

    final

--- js/Rhythm.js
+++ js/Rhythm.js
@@ -1,5 +1,8 @@
 !(function ($) {
 
+
+	var p = 0;
+	console.log('p', p)
 	document.documentElement.style.overflowY = 'hidden';
 
 	var Rhythm = function (element, options) {
@@ -9,13 +12,23 @@
 		this.targetBtn = $('#target-btn');
 		this.sizey = 2; // 背景由小變大
 		this.scroe = 0; // 分數
+		this.good = 0;
+		this.perface = 0;
+		this.miss = 0;
+		this.bad = 0;
+		this.max_comble = 0;
+		this.comble = 0;
 		this.number = null; // 目標數
 		this.sound = null; // 控制音樂
 		this.num = 0; // 分數
 		this.sy = 10; // Y軸速度
 
 		this.init(this.element);
+
+
 	}
+	var stime = 50;
+
 
 	// 初始化
 	Rhythm.prototype.init = function (element) {
@@ -31,43 +44,55 @@
 				var _scroeH = '<div id="score" class="score">分數：<span>' + _self.scroe + '</span></div>',
 					_countdowmH = ' <div class="countdown">' + _self.options.Countdown + '</div>';
 				element.append(_scroeH).append(_countdowmH);
+
+				var _comble = '<div id="com" class="c" ><span>' + _self.comble + '</span></div>',
+					_max = '<div id="com1" class="c1">最高連擊數:<span>' + _self.max_comble + '</span></div>';
+				element.append(_comble).append(_max);
+
 				_self.cuntdown();
 				startGame = null;
 			}
+
+
+
+
 			setTimeout(startGame, 1000)
 		}
 		startGame();
 	}
 
 	//遊戲開始
+
 	Rhythm.prototype.start = function () {
+
+
 		var _self = this;
 
 		var startAuto = function () {
 			_self.move();
+
 			if (_self.options.Countdown == 1) {
 
-				if (_self.scroe < 20) {
-					var _html = '<a href="javascript:;" class="close">x</a>' + '<p>請加把勁</p>' + '<P>你獲得</P>'  + '<P>'+ _self.scroe + '分</P>';
-					
-				} else if (_self.scroe > 20 && _self.scroe < 40) {
-					var _html = '<a href="javascript:;" class="close">x</a>' + '<p>Wow</p>' + '<P>恭喜你</P>' + '<P >獲得' + _self.scroe + '分</P>';
+				$('.target').hide();
+				$('.startGame').hide();
 
-				} else if(_self.scroe >40) {
-					var _html = '<a href="javascript:;" class="close">x</a>' + '<p>你超厲害的</p>' + '<P>恭喜你</P>' + '<P >獲得' + _self.scroe + '分</P>';
+				var _html = '<a href="javascript:;" class="close">x</a>' + '<P>恭喜你獲得' + _self.scroe + '分</P>' + '<P>perfect: ' + _self.perface + '<P>good:' + _self.good + '<P>bad:' + _self.bad + '<P>miss:' + _self.miss + '<P>最多連擊數:' + _self.max_comble;
 
-				}
 				$('.msg').html(_html).show();
+				
 
 				$('.mask').show();
 
 				$('.msg').on('click', 'a.close', function (event) {
 					$(this).parent().html('').hide();
 					$('.mask').hide();
-					window.location = window.location;
+					window.location.href = 'index.html';
+					// window.location = window.location;
 				});
 				_self.start = null;
+				
 				this.sound.pause();
+
 			}
 			setTimeout(startAuto, _self.options.startT)
 		}
@@ -87,7 +112,9 @@
 	}
 
 	//目標落下
+
 	Rhythm.prototype.move = function () {
+
 		var _self = this;
 		var idNum = _self.randoms();
 		var y = 0; //y軸
@@ -96,21 +123,53 @@
 			btnH = _self.targetBtn.find('.btn').height() / 2;
 
 		var time = setInterval(function () {
-			y += _self.sy;
-			size += _self.sizey;
+			if (y < thisH + btnH) {
+				y += _self.sy;
+			}
+
+			if (size <= 80) {
+				size += _self.sizey;
+			}
+
 			$('#target' + idNum).css({
 				'top': y + 'px',
 				'background-size': size + '%'
 			});
+
+
+
 			if (y > thisH + btnH) {
 				var shorT = setTimeout(function () {
+					if ($('#target' + idNum).offset().top >= 700) {
+
+						$('#m').show()
+						setTimeout(function () {
+							$("#m").hide();
+						}, stime);
+
+					
+						_self.miss = _self.miss + 1;
+		
+						if (_self.comble > _self.max_comble) {
+							_self.max_comble = _self.comble;
+						}
+						_self.comble = 0;
+						$('#com').find('span').html(_self.comble);
+						$('#com1').find('span').html(_self.max_comble);
+					}
 					$('#target' + idNum).remove();
 					shorT = null;
+
 				}, 200)
 				clearInterval(time);
+
 			}
+
 			time = null;
 		}, _self.options.speed)
+		// alert(thisH + btnH)
+
+
 
 	}
 
@@ -124,28 +183,105 @@
 			touchStatus = true;
 		}
 		var Eventfun = function () {
-			console.log('evntfun start')
+			// console.log('evntfun start')
 			var _left = $(this);
 			var index = _left.index() + 1;
 			var subTarget = $('.track' + index).find('.target');
+
+			var pd = 120;
+			var pd2 = pd - 10;
+			var pd3 = pd2 - 10;
+
+			// console.log(pd, pd2, pd3)
 			subTarget.each(function (page, elem) {
-				if ($(elem).offset().top > _left.offset().top - _left.height() / 2 - 30) {
-					_left.addClass('btnHigh');
-					$(elem).remove();
-					$('.track' + index).addClass('trackHigh');
-					setTimeout(function () {
-						_left.removeClass('btnHigh');
-						$('.track' + index).removeClass('trackHigh');
-						
-					}, 100)
-					_self.scroe++;
+
+
+				var distance = $(elem).offset().top - (_left.offset().top - _left.height())
+				// console.log($('#1').offset().top, distance)
+				if (distance > pd3) {
+
+
+
+					if (distance >= pd) {
+						p = 1;
+						$('#p').show();
+						setTimeout(function () {
+							$("#p").hide();
+						}, stime);
+
+						_self.scroe = _self.scroe + 5;
+						_self.perface = _self.perface + 1;
+						_self.comble = _self.comble + 1;
+						// console.log(_self.comble)
+						_left.addClass('btnHigh');
+						$(elem).remove();
+						p = 0;
+						$('.track' + index).addClass('trackHigh');
+						setTimeout(function () {
+							_left.removeClass('btnHigh');
+							$('.track' + index).removeClass('trackHigh');
+
+						}, 100)
+					} else if (distance >= pd2) {
+						p = 1;
+						$('#g').show();
+						setTimeout(function () {
+							$("#g").hide();
+						}, stime);
+
+
+
+						_self.scroe = _self.scroe + 3;
+						_self.good = _self.good + 1;
+						_self.comble = _self.comble + 1;
+						// console.log(_self.comble)
+
+						_left.addClass('btnHigh');
+						$(elem).remove();
+						p = 0;
+						$('.track' + index).addClass('trackHigh');
+						setTimeout(function () {
+							_left.removeClass('btnHigh');
+							$('.track' + index).removeClass('trackHigh');
+
+						}, 100)
+					} else if (distance >= pd3) {
+						p = 1;
+						$('#b').show();
+						setTimeout(function () {
+							$("#b").hide();
+						}, stime);
+						_self.scroe = _self.scroe + 1;
+						_self.bad = _self.bad + 1;
+						_self.comble = _self.comble + 1;
+						// console.log(_self.comble)
+
+						_left.addClass('btnHigh');
+						$(elem).remove();
+						p = 0;
+						$('.track' + index).addClass('trackHigh');
+						setTimeout(function () {
+							_left.removeClass('btnHigh');
+							$('.track' + index).removeClass('trackHigh');
+
+						}, 100)
+
+					}
+					if (_self.comble > _self.max_comble) {
+						_self.max_comble = _self.comble;
+					}
+
+
+					$('#com1').find('span').html(_self.max_comble);
 					$('#score').find('span').html(_self.scroe);
+					$('#com').find('span').html(_self.comble);
 				}
 			})
 		}
 		if (touchStatus) {
 
 			_self.targetBtn.on('touchstart', '.btn', Eventfun);
+
 
 
 		} else {
@@ -162,10 +298,12 @@
 				$('.btn').get(0).click();
 				// console.log('按下了D');
 				return;
+
 			case 70:
 				$('#d2')[0].play()
 				$('#target-btn').get(0).click();
 				$('.btn2').get(0).click();
+				
 				// console.log('按下了F');
 				return;
 			case 74:
@@ -183,6 +321,10 @@
 
 				// console.log('按下了K');
 				return;
+			case 32:
+
+				alert('離開遊戲回主頁')
+				window.location.href = 'index.html';
 
 		}
 
@@ -209,6 +351,7 @@
 				_CountdownT = null;
 			} else {
 				$('.countdown').html(_self.options.Countdown);
+				
 			}
 			setTimeout(_CountdownT, 1000)
 		};
@@ -217,7 +360,7 @@
 
 	$.fn.Rhythm = function (options) {
 
-		
+
 		var element = this;
 		var opts = $.extend({}, $.fn.Rhythm.defaults, options);
 		return this.each(function () {
@@ -230,7 +373,7 @@
 		musicUrl: '', //音樂
 		speed: 100, //下落速度
 		startT: 1000, //目標出現時間
-		Countdown: 60 //遊戲長度
+		Countdown: 30 //遊戲長度
 	};
 
 }(Zepto))
